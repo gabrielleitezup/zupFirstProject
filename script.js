bd40b6f2769609d14cab2757ffd03f836f84b03b
Gabriel Leite <gabriel.leite@zup.com.br>
2018-09-09 17:44:49 -0300

    Função POST City pronta!

--- script.js
+++ script.js
@@ -1,33 +1,61 @@
-function getCities() {
-    axios.get('https://customers-challenge.herokuapp.com/cities').then(function (response) {
-        const data = response.data._embedded.cities;
-        console.log(data);
-        document.getElementById("tabela").innerHTML = data.map(function (city) {
-            return (
-                '<tr>' +
-                '<td>' + city.name + '</td>' +
-                '</tr>'
-            );
-        }).join('');
-    }).catch(function (error) {
-        console.log(error);
-    });
-}
-getCities();
+// function getCities() {
+//     axios.get('https://customers-challenge.herokuapp.com/cities').then(function (response) {
+//         const data = response.data._embedded.cities;
+//         console.log(data);
+//         document.getElementById("tabela").innerHTML = data.map(function (city) {
+//             return (
+//                 '<tr>' +
+//                 '<td>' + city.name + '</td>' +
+//                 '</tr>'
+//             );
+//         }).join('');
+//     }).catch(function (error) {
+//         console.log(error);
+//     });
+// }
+// //getCities();
 
-function getCustomers() {
-    axios.get('https://customers-challenge.herokuapp.com/customers').then(function (response) {
-        const data = response.data._embedded.customers;
-        document.getElementById("tabclientes").innerHTML = data.map(function (cliente) {
-            return (
-                '<tr>' +
-                '<td>' + cliente.name + '</td>' +
-                '</tr>'
-            );
-        }).join('')
-    }).catch(function (error) {
-        console.log(error);
-    });
-}
+// function getCustomers() {
+//     axios.get('https://customers-challenge.herokuapp.com/customers').then(function (response) {
+//         const data = response.data._embedded.customers;
+//         console.log(data);
+//         document.getElementById("tabclientes").innerHTML = data.map(function (cliente) {
+//             return (
+//                 '<tr>' +
+//                 '<td>' + cliente.name + '</td>' +
+//                 '</tr>'
+//             );
+//         }).join('')
+//     }).catch(function (error) {
+//         console.log(error);
+//     });
+// }
 
-getCustomers();
+// //getCustomers();
+
+function postCity() {
+    document.getElementById('post').onclick = function() {
+        var data = document.getElementById('city').value;
+        var jsonData = JSON.stringify({ name: (data) });
+        var cityJSON = JSON.parse(jsonData);
+
+            axios.post('https://customers-challenge.herokuapp.com/cities', cityJSON)
+                .then(function(response) {
+                    console.log(response);
+                    alert("Cidade cadastrada com sucesso!");
+                })
+                .catch(function(error) {
+                    console.log(error);
+                });
+    };
+};
+
+postCity();
+
+// function postCity() {
+//     axios.post('https://customers-challenge.herokuapp.com/cities', ).then(function (response){
+//         console.log(response);
+//     }).catch(function(error){
+//         console.log(error);
+//     })
+// }
